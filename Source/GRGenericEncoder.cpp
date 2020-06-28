f2347bde4108a39bba0ecfbba1c9fea4a486bdfa
Nicholas Corgan <n.corgan@gmail.com>
2020-06-28 12:40:25 -0500

    work() calls encoder for as many frames as possible, keeps slowing down

--- Source/GRGenericEncoder.cpp
+++ Source/GRGenericEncoder.cpp
@@ -15,18 +15,30 @@ GRGenericEncoder::~GRGenericEncoder(){}
 
 void GRGenericEncoder::work()
 {
-    const auto minSize = std::max(_genericEncoderSPtr->get_input_size(), _genericEncoderSPtr->get_output_size());
+    const auto inputCoderSize = _genericEncoderSPtr->get_input_size();
+    const auto outputCoderSize = _genericEncoderSPtr->get_output_size();
+    const auto maxCoderSize = std::max(inputCoderSize, outputCoderSize);
 
     const auto elems = this->workInfo().minElements;
-    if(elems < static_cast<size_t>(minSize)) return;
+    if(elems < static_cast<size_t>(maxCoderSize)) return;
 
     auto input = this->input(0);
     auto output = this->output(0);
 
-    _genericEncoderSPtr->generic_work(
-        input->buffer(),
-        output->buffer());
+    auto inBuff = input->buffer().as<std::uint8_t*>(); // GR needs it non-const, but it won't be updated.
+    auto outBuff = output->buffer().as<std::uint8_t*>();
 
-    input->consume(_genericEncoderSPtr->get_input_size());
-    output->produce(_genericEncoderSPtr->get_output_size());
+    const auto numFrames = elems / maxCoderSize;
+    for(size_t frameIndex = 0; frameIndex < numFrames; ++frameIndex)
+    {
+        _genericEncoderSPtr->generic_work(
+            input->buffer(),
+            output->buffer());
+
+        inBuff += inputCoderSize;
+        outBuff += outputCoderSize;
+    }
+
+    input->consume(_genericEncoderSPtr->get_input_size() * numFrames);
+    output->produce(_genericEncoderSPtr->get_output_size() * numFrames);
 }
